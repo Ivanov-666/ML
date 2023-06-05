9ce7e02ba0497d47ad37b564332ac091ad1d95a1
Ivanov-666 <ivanovf2711@gmail.com>
2023-06-05 17:34:38 +0600

    ЛР8 финал

--- mylib/PCA.py
+++ mylib/PCA.py
@@ -8,7 +8,8 @@ class PCA:
         X = stsc.fit_transform(X)
         cov_matrix = np.cov(X.T)
         values, vectors = np.linalg.eigh(cov_matrix)
-        pairs = sorted(zip(values, vectors))[:self.n]
+        pairs = list(zip(values, vectors))
+        pairs = sorted(pairs, key=lambda x: (x[0],x[1][0],x[1][1]))[:self.n]
         pairs = map(lambda a: a[1], pairs)
         principle_data = np.dot(X, np.array(list(pairs)).T)
         return principle_data
